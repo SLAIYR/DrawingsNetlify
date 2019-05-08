66af2cc57922f131f6c5ef1ba14c08fee6352c28
SamanthaLaiyoeurn <naty_coul@hotmail.fr>
2019-05-08 22:28:07 +0200

    h1 decoupe dans un autre composant

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Canvas from './canvas';
+import Hello from './hello'
 
 class App extends React.Component 
 {
@@ -7,8 +8,7 @@ class App extends React.Component
     {
         return (
           <React.Fragment>
-            <h1>Draw me something!</h1>
-            
+                <Hello />
                 <Canvas />
           </React.Fragment>
         
---
+++ src/hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.PureComponent 
+{
+    render() 
+    {
+        return (
+            <h1> Draw me something! </h1>
+        );
+    }
+}
+    
+export default Hello;
